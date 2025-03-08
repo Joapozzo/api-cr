090eba02328dc3df9e07c8b88916d76f87d8e4c7
Joa Pozzo <pozzojoa@gmail.com>
2025-03-08 15:32:57 -0300

    .

--- index.js
+++ index.js
@@ -63,6 +63,20 @@ app.use(cors({
     credentials: true
 }));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+        return res.status(204).end();
+    }
+    
+    next();
+});
+
+
 // Manejar preflight requests (OPTIONS)
 app.options("*", (req, res) => {
     res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
